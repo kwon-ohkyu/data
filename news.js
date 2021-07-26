7fa5f111ef47ad7f13a6c022ac991072e2b2bdce
kwon-ohkyu <53881929+kwon-ohkyu@users.noreply.github.com>
2021-07-26 21:14:43 +0900

    Add files via upload

--- news.js
+++ news.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 const cheerio = require('cheerio')
-const parseString = require('xml2js').parseString;
 const fs = require('fs');
+
+const searchWords = ['african+swine+fever', 'Foot+and+mouth+disease', 'Highly+pathogenic+avian+influenza'];
 
 let URL = 'https://medisys.newsbrief.eu/rss/?type=search&mode=advanced&exact=african+swine+fever&language=en'
 
@@ -17,19 +18,22 @@ const getHTML = async(URL) =>{
 }
 
 
-getHTML(URL).then(res=>{
+function getData(URL){
     let json = [];
-    const $ = cheerio.load(res)
-    const wantdata = $('item');
-    wantdata.each((idx,elem)=>{
-        title = elem.children[0].children[0].data
-        link = elem.children[7].attribs['url']
-        country =  elem.children[7].attribs['country']
-        date = elem.children[5].children[0]['data']
-        // fs.writeFile('1.txt',elem.text(),function(){
-        json.push({title:title, link:link, country:country, date:date})
-        })
-    console.log('json:',json)
-    });
+    getHTML(URL).then(res=>{
+        const $ = cheerio.load(res)
+        const wantdata = $('item');
+        wantdata.each((idx,elem)=>{
+            title = elem.children[0].children[0].data
+            link = elem.children[7].attribs['url']
+            country =  elem.children[7].attribs['country']
+            date = elem.children[5].children[0]['data']
+            // fs.writeFile('1.txt',elem.text(),function(){
+            json.push({title:title, link:link, country:country, date:date})
+            })
+        console.log('json:',json)
+        });
+        return json
+    }
 
-
+getData(URL)
